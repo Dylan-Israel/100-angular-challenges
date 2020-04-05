e995d0e40a93b523a40312fd31441ade6d14c941
Dylan <dylansemail310@gmail.com>
2020-04-05 11:33:42 -0400

    Added basic toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ToolbarComponent {
+  @Input() public title = '';
 
 }
