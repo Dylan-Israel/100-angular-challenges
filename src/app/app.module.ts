21630ccd47d8d54ecc84e90bc3237feaf7e36c85
Dylan <dylansemail310@gmail.com>
2020-04-05 10:55:03 -0400

    Added toolbar setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/toolbar/README.md
@@ -0,0 +1,9 @@
+# Toolbar Component
+
+Build a basic toolbar component that renders out the content nested within it and a title passed in.
+
+## Tips
+---
+- component
+- ng-content
+- @Input()
