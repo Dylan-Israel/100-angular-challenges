e4b279271620e224870a8f5ba9ee9020258056d9
Dylan <dylansemail310@gmail.com>
2020-05-17 10:18:14 -0400

    added recorded paging

--- src/app/components/paging/paging.component.ts
+++ src/app/components/paging/paging.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-paging',
@@ -10,54 +11,55 @@ export class PagingComponent {
   @Input() public pageSize = 25;
   @Input() public currentPage = 1;
   @Input() public step = 2;
-  @Output() public pageChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() public pageChange: EventEmitter<number> = new EventEmitter();
 
-  public get totalPages() {
+  public get totalPages(): number {
     return Math.ceil((this.totalItems || 1) / (this.pageSize || 1));
   }
 
-  public get isFirstPage() {
+  public get isFirstPage(): boolean {
     return this.currentPage === 1;
   }
 
-  public get isLastPage() {
+  public isLastPage(): boolean {
     return this.currentPage === this.totalPages;
   }
 
-  public get isPreviousDisabled() {
+  public get isPreviousDisabled(): boolean {
     return this.currentPage - this.step < 1;
   }
 
-  public get isNextDisabled() {
+  public get isNextDisabled(): boolean {
     return this.currentPage + this.step > this.totalPages;
   }
 
-  public get pages() {
-    const result = [];
+  public get pages(): number[] {
+    const pages = [];
 
     if (this.currentPage > 1) {
-      result.push(this.currentPage - 1);
+      pages.push(this.currentPage - 1);
     }
 
-    result.push(this.currentPage);
+    pages.push(this.currentPage);
 
     if (this.currentPage < this.totalPages) {
-      result.push(this.currentPage + 1);
+      pages.push(this.currentPage + 1);
     }
 
     if (this.currentPage + 1 < this.totalPages) {
-      result.push(this.currentPage + 2);
+      pages.push(this.currentPage + 2);
     }
 
-    if (this.currentPage + 2 < this.totalPages && this.currentPage === 1) {
-      result.push(this.currentPage + 3);
+    if (this.currentPage + 1 < this.totalPages && this.currentPage === 1) {
+      pages.push(this.currentPage + 3);
     }
 
-    return result;
+    return pages;
   }
 
-  public setPage(page: number) {
+  public setPage(page: number): void {
     this.currentPage = page;
     this.pageChange.emit(page);
   }
+
 }
