78a79aeaf784e606c17535d55733ff935191355e
Dylan <dylansemail310@gmail.com>
2020-05-22 18:02:37 -0400

    added tabs

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -9,11 +9,10 @@ export class TabsComponent {
   @Input() public tabs: { title: string, active: boolean }[] = [];
   @Output() public selectedTabIndex = new EventEmitter<number>();
 
-  public selectTab(selectedTab: { title: string, active: boolean }, index: number) {
-    this.tabs.forEach(tab => {
-      tab.active = selectedTab === tab;
+  public selectTab(selected: { title: string, active: boolean }, index: number): void {
+    this.tabs.forEach((tab) => {
+      tab.active = (selected === tab);
     });
     this.selectedTabIndex.emit(index);
   }
-
 }
