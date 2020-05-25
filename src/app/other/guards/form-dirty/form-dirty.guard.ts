9ba9863c120015d80291e86838ba879cf12f4998
Dylan <dylansemail310@gmail.com>
2020-05-24 22:36:30 -0400

    added form dirty guard recording

--- src/app/other/guards/form-dirty/form-dirty.guard.ts
+++ src/app/other/guards/form-dirty/form-dirty.guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
 import { OtherDocumentationComponent } from '../../other-documentation/other-documentation.component';
+import { IFormDirty } from './form-dirty.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FormDirtyGuard implements CanDeactivate<OtherDocumentationComponent> {
-  canDeactivate(component: OtherDocumentationComponent): boolean {
+export class FormDirtyGuard implements CanDeactivate<IFormDirty> {
+  public canDeactivate(component: IFormDirty): boolean {
     if (component.form.dirty) {
-      return confirm('Your form is dirty are you sure you want to leave?');
+      return confirm('Your form is dirty. Are you sure you want to leave?');
     }
 
     return true;
   }
-
 }
