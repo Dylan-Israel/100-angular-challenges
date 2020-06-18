348e68c8d660594cfcf46adad0cef380a4bb24a1
Dylan <dylansemail310@gmail.com>
2020-06-18 18:07:18 -0400

    added password recording

--- src/app/components/password/password.component.ts
+++ src/app/components/password/password.component.ts
@@ -6,53 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./password.component.scss']
 })
 export class PasswordComponent {
-  public value = 0;
+  public passwordStrengthPercent = 0;
 
   private currentPassword = '';
+
   public set password(value: string) {
     this.currentPassword = value;
-    this.updatePasswordValue(this.currentPassword);
+    this.updatePasswordStrength(value);
   }
 
   public get password(): string {
     return this.currentPassword;
   }
 
-  public get is8CharsOrGreater(): boolean {
+  public get is8CharactersOrGreater(): boolean {
     return this.currentPassword.length >= 8;
   }
 
-  public get hasOneCapitalLetter(): boolean {
+  public get hasOneCapitalLetterOrMore(): boolean {
     return /(.*[A-Z].*)/.test(this.currentPassword);
   }
 
-  public get hasANumber(): boolean {
+  public get hasOneNumberOrMore(): boolean {
     return /(.*\d.*)/.test(this.currentPassword);
   }
 
-  public get hasASpecialCharacter(): boolean {
+  public get hasOneSpecialCharacterOrMore(): boolean {
     return /\W|_/g.test(this.currentPassword);
   }
 
-  private updatePasswordValue(value: string) {
-    let count = 0;
+  private updatePasswordStrength(value: string): void {
+    let passwordStrength = 0;
 
-    if (this.is8CharsOrGreater) {
-      count += 25;
+    if (this.is8CharactersOrGreater) {
+      passwordStrength += 25;
     }
 
-    if (this.hasOneCapitalLetter) {
-      count += 25;
+    if (this.hasOneCapitalLetterOrMore) {
+      passwordStrength += 25;
     }
 
-    if (this.hasANumber) {
-      count += 25;
+    if (this.hasOneNumberOrMore) {
+      passwordStrength += 25;
     }
 
-    if (this.hasASpecialCharacter) {
-      count += 25;
+    if (this.hasOneSpecialCharacterOrMore) {
+      passwordStrength += 25;
     }
 
-    this.value = count;
+    this.passwordStrengthPercent = passwordStrength;
   }
 }
