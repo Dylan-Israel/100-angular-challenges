9bf649816357348c4ede330948fa80b37a8be96d
Dylan <dylansemail310@gmail.com>
2020-07-11 20:18:50 -0400

    added rich text pipe testing

--- src/app/pipes/rich-text/rich-text.pipe.spec.ts
+++ src/app/pipes/rich-text/rich-text.pipe.spec.ts
@@ -1,8 +1,51 @@
 import { RichTextPipe } from './rich-text.pipe';
 
-describe('RichTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RichTextPipe();
-    expect(pipe).toBeTruthy();
+describe(RichTextPipe.name, () => {
+  it('Exists', () => {
+    // assert
+    expect(RichTextPipe).toBeDefined();
+  });
+
+  describe('General', () => {
+    let pipe: RichTextPipe;
+
+    beforeEach(() => {
+      pipe = new RichTextPipe();
+    });
+
+    describe('transform', () => {
+      it('non valid html returns empty', () => {
+        // arrange
+        const value = '<a> 12334';
+
+        // act
+        const result = pipe.transform(value);
+
+        // assert
+        expect(result).toBe('');
+      });
+
+      it('empty html returns an empty string', () => {
+        // arrange
+        const value = '<a></a>';
+
+        // act
+        const result = pipe.transform(value);
+
+        // assert
+        expect(result).toBe('');
+      });
+
+      it('valid non empty html returns the html', () => {
+        // arrange
+        const value = '<button>Hello World</button>';
+
+        // act
+        const result = pipe.transform(value);
+
+        // assert
+        expect(result).toBe(value);
+      });
+    });
   });
 });
